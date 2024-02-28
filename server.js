85e54da6e2310be932a37c8b36f0dff5ddf38de2
earnt <tinnaearn@gmail.com>
2024-02-28 20:35:27 +0700

    Add user code

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
+
 // const company = require('./routes/companyRoutes');
 // const session = require('./routes/sessionRoutes');
-// const user = require('./routes/userRoutes');
+const user = require('./routes/userRoutes');
 
 dotenv.config({ path: './config/config.env' });
 
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/v1/auth', user);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(
