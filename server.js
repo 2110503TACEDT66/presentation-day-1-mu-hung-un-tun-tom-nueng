bf659072932eba4d36ab999a839ea641fe35bb99
datswhaaan <145758336+datswhaaan@users.noreply.github.com>
2024-03-02 20:41:19 +0000

    Add helmet package for HTTP security headers

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const connectDB = require('./config/db');
 const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
 const company = require('./routes/companyRoutes');
 const session = require('./routes/sessionRoutes');
@@ -16,6 +17,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(mongoSanitize());
+app.use(helmet());
 
 app.use('/companies', company);
 app.use('/auth', user);
