4eccdf5b0d521549b8e61da560d4bd729e39e7bf
datswhaaan <145758336+datswhaaan@users.noreply.github.com>
2024-03-03 15:01:28 +0000

    modify controller and model for company

--- models/Company.js
+++ models/Company.js
@@ -1,28 +1,51 @@
 const mongoose = require('mongoose');
 
-const CompanySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, `Please add company's name`],
-    unique: true,
-    trim: true,
+const CompanySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, `Please add company's name`],
+      unique: true,
+      trim: true,
+    },
+    address: {
+      type: String,
+      required: [true, `Please add company's address`],
+    },
+    website: {
+      type: String,
+      required: [true, `Please add company's website`],
+    },
+    desc: {
+      type: String,
+      required: [true, `Please add company's description`],
+    },
+    tel: {
+      type: String,
+      required: [true, `Please add company phone number`],
+    },
   },
-  address: {
-    type: String,
-    required: [true, `Please add company's address`],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  website: {
-    type: String,
-    required: [true, `Please add company's website`],
-  },
-  desc: {
-    type: String,
-    required: [true, `Please add company's description`],
-  },
-  tel: {
-    type: String,
-    required: [true, `Please add company phone number`],
-  },
+);
+
+CompanySchema.virtual('sessions', {
+  ref: 'Session',
+  localField: '_id',
+  foreignField: 'company_id',
+  justOne: false,
 });
+
+CompanySchema.pre(
+  'deleteOne',
+  { document: true, query: false },
+  async function (next) {
+    console.log(`Sessions being removed from company ${this._id}`);
+    await this.model('Session').deleteMany({ hospital: this._id });
+    next();
+  },
+);
 
 module.exports = mongoose.model('Company', CompanySchema);
