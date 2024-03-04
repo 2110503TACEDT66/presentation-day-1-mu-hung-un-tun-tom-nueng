a1d0002ce74109fd2946cde97de739de094e8aab
earnt <tinnaearn@gmail.com>
2024-03-04 14:46:27 +0700

    Edit addSession response

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -116,7 +116,7 @@ exports.addSession = async (req, res, next) => {
     }
 
     const session = await Session.create(req.body);
-    res.status(200).json({
+    res.status(201).json({
       success: true,
       data: session,
     });
